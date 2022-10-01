bad8f5fcb20706f59a02c52f67bd7faee22d2dee
ryo ishii <45378620+ryoishii1106@users.noreply.github.com>
2022-10-01 19:20:34 +0900

    build webpacker container

--- Dockerfile
+++ Dockerfile
@@ -21,7 +21,7 @@ RUN apt-get update -qq && apt-get install -y curl apt-transport-https wget && \
 # 作業ディレクトリの作成、設定
 RUN mkdir /app_name 
 ##作業ディレクトリ名をAPP_ROOTに割り当てて、以下$APP_ROOTで参照
-ENV APP_ROOT /app_name 
+ENV APP_ROOT /app_name
 WORKDIR $APP_ROOT
 
 # ホスト側（ローカル）のGemfileを追加する（ローカルのGemfileは【３】で作成）
--- app/javascript/MainHeader.vue
+++ app/javascript/MainHeader.vue
@@ -7,7 +7,7 @@
       </button>
 
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto">
+        <ul class="navbar-nav ms-auto">
           <li class="nav-item active">
             <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
           </li>
